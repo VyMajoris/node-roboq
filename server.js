aee1b6cbff022bcd5897b981086e9ce211f777c2
Eduardo Araujo <vycanismajoriscsa@gmail.com>
2016-09-02 23:59:04 -0300

    work

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 'use strict';
 
 const express = require('express');
-
+const qr = require('qr-image');
+const crypto = require('crypto');
+  var text = 'I love cupcakes'
+  var key = 'abcdeg'
+  var hash
+ 
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+    var current_date = (new Date()).valueOf().toString();
+    var random = Math.random().toString();
+    hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
+ console.log(hash)
+ 
+
+  res.send( qr.imageSync(hash, { type: 'svg', size: 10}));
 });
 
 app.listen(PORT);
