c761fdeff0dcb560a8c540dab2fdca5cd148669c
Eduardo Araujo <vycanismajoriscsa@gmail.com>
2016-09-04 20:19:39 -0300

    node

--- server.js
+++ server.js
@@ -1,26 +1,65 @@
 'use strict';
 
-const express = require('express');
+
 const qr = require('qr-image');
 const crypto = require('crypto');
-  var text = 'I love cupcakes'
-  var key = 'abcdeg'
-  var hash
- 
-// Constants
+const request = require('request');
+const firebase = require('firebase');
+const express = require('express');
+var bodyParser = require('body-parser')
+
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+app.use( bodyParser.json() );  
+
 const PORT = 8080;
 
-// App
-const app = express();
-app.get('/', function (req, res) {
-    var current_date = (new Date()).valueOf().toString();
-    var random = Math.random().toString();
-    hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
- console.log(hash)
- 
+const fireapp = firebase.initializeApp({
+    apiKey: "AIzaSyALJYmRMRfMxMXYFCGT8yStxN2xzh-xoPY",
+    authDomain: "roboq-7e9fe.firebaseapp.com",
+    databaseURL: "https://roboq-7e9fe.firebaseio.com",
+    storageBucket: "roboq-7e9fe.appspot.com",
+  });
+ var database = firebase.database();
+ var roboQRef = firebase.database().ref('estbXYZ/fila')
 
-  res.send( qr.imageSync(hash, { type: 'svg', size: 10}));
+
+
+var inboundHash;
+app.get('/start', function (req, res) {
+    var hash = crypto.createHash('sha1').update((new Date()).valueOf().toString() + Math.random().toString()).digest('hex');
+    roboQRef.set({
+        'hash':hash
+    });
+
+
+  res.send( qr.imageSync(hash, { size: 10}));
 });
+
+app.post('/get', function (req, res) {
+    var result = fireGet(roboQRef)
+    console.log(result)
+});
+
+app.post('/auth', function (req, res) {
+    console.log(req)
+    inboundHash =  req.body.hash;
+    roboQRef.once('value',performAuth);
+});
+
+function performAuth(snapshot){
+    console.log('callback',snapshot.val().hash)
+    if (inboundHash == snapshot.val().hash){
+        console.log("AAAAAAAAAA")
+    }else{
+        console.log("bbbbbbbbb")
+    }
+}
+
+
+
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
