cac9a0a0d326d9bc0b2a01bcf01ef4c74756c31c
Eduardo Araujo <vycanismajoriscsa@gmail.com>
2016-09-15 21:42:40 -0300

    made changes

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.post('/forfeitTicket', function (req, res) {
 });
 app.post('/auth', function (req, res) {
     console.log(req.body)
-    console.log(JSON.parse(req.body).queuePosID)
+    console.log(JSON.parse(req.body))
     queuePosID = JSON.parse(req.body).queuePosID;
     roboQQueuersRef.child(queuePosID).once('value', function (snapshot) {
         if (snapshot.val().pos == 1) {
